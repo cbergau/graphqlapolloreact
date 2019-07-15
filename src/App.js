c6aa392446b0ccdab0bfa24c0200f8af76c502ca
Christian Bergau <cbergau86@gmail.com>
2019-07-15 23:58:54 +0200

    basket

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import gql from 'graphql-tag';
+import {Query} from 'react-apollo';
+
+const GET_BASKET = gql`
+    {
+        basketById(basketId: "7953FCFB-A98E-4689-A0A0-65C069C99000") {
+            basketId
+            basketValueWithShippingFormatted
+            positions {
+                positionValueFormatted
+                article {
+                    ean
+                }
+            }
+        }
+    }
+`;
+
+const Basket = () => (
+    <Query query={GET_BASKET}>
+        {({loading, error, data}) => {
+            if (loading) return 'Loading...';
+            if (error) return `Error! ${error.message}`;
+
+            return (
+                <div>
+                <ul>
+                    {data.basketById.positions.map(position => (
+                        <li>{position.article.ean}</li>
+                    ))}
+                </ul>
+                    <b>Sum: {data.basketById.basketValueWithShippingFormatted}</b>
+                </div>
+            );
+        }}
+    </Query>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Basket/>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {ApolloProvider} from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {ApolloClient} from 'apollo-client';
+import {HttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const client = new ApolloClient({
+    link: new HttpLink({uri: "http://localhost:8080/graphql"}),
+    cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>, document.getElementById('root'));
+
 serviceWorker.unregister();
